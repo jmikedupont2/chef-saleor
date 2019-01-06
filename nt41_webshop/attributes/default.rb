ada2a90ad5339985facf342ccbe5a0e5dbda13ef
j. mike dupont <jmikedupont2@gmail.com>
2019-01-06 09:14:39 -0500

    v1

---
+++ nt41_webshop/attributes/default.rb
@@ -0,0 +1,7 @@
+default['database']['user_name']='webshop'
+default['database']['user_password']= 'hyp(OgTar1'
+default['database']['database_name']= default['database']['user_name']
+default['database']['database_host']= 'localhost'
+#default['poise-python']['database_host']= 'localhost'
+
+
